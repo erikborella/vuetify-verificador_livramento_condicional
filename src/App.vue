614e8f57cb6a9e29ae87e4689702ba635fd98a00
Erik Borela <erikborella@gmail.com>
2021-06-13 21:38:10 -0300

    footer

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,18 @@
       <!-- <HelloWorld/> -->
       <VerificadorLivramentoCondicional/>
     </v-main>
+
+    <v-footer color="primary darken-1">
+      <v-toolbar color="primary darken-1" flat dense>
+        <v-toolbar-title class="text-body-1 white--text">
+          {{ new Date().getFullYear() }} - <strong>Erik Borela</strong>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn icon target="_blank" href="https://github.com/erikborella/vuetify-verificador_livramento_condicional">
+          <v-icon color="white">mdi-github</v-icon>
+      </v-btn>
+      </v-toolbar>
+    </v-footer>
   </v-app>
 </template>
 
